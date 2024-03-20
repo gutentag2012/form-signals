4ddbe1d68ad32e39cc0ce5b316b4d0362ed9021f
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-03-20 22:27:04 +0100

    Add helper to get all dirty fields of an object

--- packages/form-core/src/utils/equality.utils.spec.ts
+++ packages/form-core/src/utils/equality.utils.spec.ts
@@ -1,7 +1,8 @@
 import { describe, expect, it } from 'vitest'
-import { equalityUtils } from './equality.utils'
+import {getLeftUnequalPaths, isEqualDeep} from './equality.utils'
 
 describe('equality.utils', () => {
+  describe('isEqualDeep', () => {
   it.each([
     [1, 1],
     [null, null],
@@ -20,7 +21,7 @@ describe('equality.utils', () => {
   ])(
     'should return true when a and b are equal for values %j and %j',
     (a, b) => {
-      expect(equalityUtils(a, b)).toBe(true)
+      expect(isEqualDeep(a, b)).toBe(true)
     },
   )
   it.each([
@@ -42,7 +43,58 @@ describe('equality.utils', () => {
   ])(
     'should return false when a and b are not equal for values %j and %j',
     (a, b) => {
-      expect(equalityUtils(a, b)).toBe(false)
+      expect(isEqualDeep(a, b)).toBe(false)
     },
   )
+  });
+
+  describe('getLeftUnequalPaths', () => {
+    it.each([
+      [1, 1],
+      [null, null],
+      [undefined, undefined],
+      [new Date(0), new Date(0)],
+      [{}, {}],
+      [{ a: 1 }, { a: 1 }],
+      [
+        { a: 1, b: { c: 2 } },
+        { a: 1, b: { c: 2 } },
+      ],
+      [
+        { nestedArray: [1, undefined, null, new Date(0)] },
+        { nestedArray: [1, undefined, null, new Date(0)] },
+      ],
+    ])(
+      "should return empty array when a and b are equal for values %j and %j",
+      (a, b) => {
+        expect(getLeftUnequalPaths(a, b)).toEqual([])
+      },
+    )
+    it("should not report additional keys from right object", () => {
+      expect(getLeftUnequalPaths({ a: 1, b: 2 }, { a: 1, b: 2, c: 3 })).toEqual([])
+    })
+    it.each([
+      [1, 2, [""]],
+      [null, undefined, [""]],
+      [undefined, null, [""]],
+      [new Date(0), new Date(1), [""]],
+      [{ a: 1 }, { a: 2 }, ["a"]],
+      [{ a: 1 }, { aa: 1 }, ["a"]],
+      [
+        { a: 1, b: { c: 2 } },
+        { a: 1, b: { c: 3 } },
+        ["b.c"],
+      ],
+      [
+        { nestedArray: [1, undefined, null, new Date(), {deeply: {nested: ["object", "missing"]}}] },
+        { nestedArray: [1, undefined, null, new Date(1), {deeply: {nested: ["objectt"]}}] },
+        ["nestedArray.3", "nestedArray.4.deeply.nested.0", "nestedArray.4.deeply.nested.1"],
+      ],
+    ])(
+      "should return array of paths when a and b are not equal for values %j and %j",
+      (a, b, expected) => {
+        expect(getLeftUnequalPaths(a, b)).toEqual(expected)
+      },
+    )
+  });
 })
--- packages/form-core/src/utils/equality.utils.ts
+++ packages/form-core/src/utils/equality.utils.ts
@@ -2,7 +2,7 @@ function isNullOrUndefined<T>(value: T): value is NonNullable<T> {
   return value === null || value === undefined
 }
 
-export function equalityUtils(a: unknown, b: unknown): boolean {
+export function isEqualDeep(a: unknown, b: unknown): boolean {
   // Primitives + object references
   if (a === b) return true
   if (isNullOrUndefined<unknown>(a) || isNullOrUndefined<unknown>(b))
@@ -23,7 +23,40 @@ export function equalityUtils(a: unknown, b: unknown): boolean {
     const valA = aNonNullable[key as keyof typeof aNonNullable]
     const valB = bNonNullable[key as keyof typeof bNonNullable]
 
-    if (!equalityUtils(valA, valB)) return false
+    if (!isEqualDeep(valA, valB)) return false
   }
   return true
+}
+
+function getLeftUnequalPathsInternal(a: unknown, b: unknown, currentKey="", acc:string[]=[]): string[] {
+  let internalAcc = acc
+  // Primitives + object references
+  if (a === b) return acc
+  if (isNullOrUndefined<unknown>(a) || isNullOrUndefined<unknown>(b))
+    return a === b ? acc : [...internalAcc, currentKey]
+  if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime() ? acc : [...internalAcc, currentKey]
+
+  if (typeof a !== 'object' || typeof b !== 'object') return [...internalAcc, currentKey]
+
+  const aNonNullable = a as NonNullable<unknown>
+  const bNonNullable = b as NonNullable<unknown>
+  const aKeys = Object.keys(aNonNullable)
+
+  for (const key of aKeys) {
+    const nextKey = currentKey ? `${currentKey}.${key}` : key
+
+    if (!(key in bNonNullable)) {
+      internalAcc  = [...internalAcc, nextKey]
+      continue
+    }
+    const valA = aNonNullable[key as keyof typeof aNonNullable]
+    const valB = bNonNullable[key as keyof typeof bNonNullable]
+
+    internalAcc = getLeftUnequalPathsInternal(valA, valB, nextKey, internalAcc)
+  }
+
+  return internalAcc
+}
+export function getLeftUnequalPaths(a: unknown, b: unknown): string[] {
+  return getLeftUnequalPathsInternal(a, b)
 }
