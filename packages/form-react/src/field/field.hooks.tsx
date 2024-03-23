8e9a10a0ca1afac40dcfa9afbedfcd75386e7f98
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-03-23 09:57:12 +0100

    Explicitly return the context from the use hooks

--- packages/form-react/src/field/field.hooks.tsx
+++ packages/form-react/src/field/field.hooks.tsx
@@ -5,13 +5,20 @@ import type {
 } from '@signal-forms/form-core'
 import React from 'react'
 import type { FormContextType } from '../form'
-import { fieldLogicToFieldContext } from './field.context'
+import {
+  type FieldContextType,
+  fieldLogicToFieldContext,
+} from './field.context'
 
-export function useField<TData, TName extends Paths<TData>, TBoundValue>(
+export function useField<
+  TData,
+  TName extends Paths<TData>,
+  TBoundValue = never,
+>(
   form: FormContextType<TData> | FormLogic<TData>,
   name: TName,
   options: FieldLogicOptions<TData, TName, TBoundValue>,
-) {
+): FieldContextType<TData, TName, TBoundValue> {
   const field = form.getOrCreateField(name, options)
   const finalField = React.useMemo(
     () => fieldLogicToFieldContext(field),
--- packages/form-react/src/form/form.hooks.tsx
+++ packages/form-react/src/form/form.hooks.tsx
@@ -1,8 +1,10 @@
 import { FormLogic, type FormLogicOptions } from '@signal-forms/form-core'
 import React from 'react'
-import { formLogicToFormContext } from './form.context'
+import { type FormContextType, formLogicToFormContext } from './form.context'
 
-export function useForm<TData>(options: FormLogicOptions<TData>) {
+export function useForm<TData>(
+  options: FormLogicOptions<TData>,
+): FormContextType<TData> {
   // biome-ignore lint/correctness/useExhaustiveDependencies: We only ever want to create a form once
   const finalForm = React.useMemo(() => {
     const form = new FormLogic(options)
