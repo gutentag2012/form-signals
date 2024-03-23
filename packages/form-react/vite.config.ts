5399e3c0af523a7bde0e2a76e98a21f235eceaf4
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-03-23 09:50:40 +0100

    Fix vite config import because of types

--- packages/form-react/vite.config.ts
+++ packages/form-react/vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
