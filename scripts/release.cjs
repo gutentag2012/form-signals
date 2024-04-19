5fd2dbcf1afc765b5f2c3599f2887a342bb43325
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-04-19 22:16:07 +0200

    chore: Only allow to release on main branch

--- scripts/release.cjs
+++ scripts/release.cjs
@@ -1,6 +1,15 @@
 const {releaseVersion, releaseChangelog} = require("nx/release")
+const {execSync} = require("node:child_process");
 
 async function run() {
+  const branchRaw = execSync('git rev-parse --abbrev-ref HEAD')
+  const branch = branchRaw.toString().trim()
+
+  if (branch !== 'main') {
+    console.error('Not on main branch, aborting release')
+    return
+  }
+
   const {workspaceVersion, projectsVersionData} = await releaseVersion({})
 
   await releaseChangelog({
